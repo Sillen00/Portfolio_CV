cd0c08c724df237ffbe84fe30c3b0c85694f3590
Simon <sillen.00@hotmail.com>
2024-05-12 16:53:53 +0200

    Line on after h3 text

--- app/containers/home-page/About.tsx
+++ app/containers/home-page/About.tsx
@@ -1,9 +1,100 @@
+import { Play } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+import css from "./About.module.scss";
+
 const About = () => {
   return (
     <div className='max-w-screen-lg mx-auto px-6 md:px-28' id='about'>
-      <h3 className='mb-8'>
+      <h3 className={twMerge(css.line, "relative hidden md:block mb-8")}>
         <span className='fira text-secondary-color text-2xl'>01. </span>About Me
       </h3>
+
+      <div className='flex gap-4 wrap '>
+        <p className='min-w-[50%] gray'>
+          Hej! Jag är en junior Frontend-Utvecklare, född och uppvuxen i Alingsås. Upplevs som en positiv kille som gärna vill hjälpa till
+          och är serviceminded. Har inga problem med nya arbetsuppgifter utan är snarare nyfiken och motiverad att utveckla mina färdigheter
+          samt lära mig nytt. Jag tycker det är roligt att samarbeta samtidigt som jag inte har problem att arbeta ensam. Jag gillar att ha
+          ordning och reda, är noggrann med de uppgifter som jag får. Har inga problem med att vara flexibel och hjälpa till där det behövs
+          som mest.
+        </p>
+
+        <div className='gray relative min-w-[50%] flex border p-6 border-secondary-color'>
+          <p className='absolute -top-3 bg-[var(--background-color)] px-2 left-4 hidden md:block'>Skills </p>
+          <ul>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Javascript</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Typescript</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>React.JS</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Npm</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>UX/UI</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Figma</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Github</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>ExpressJS</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Mongodb</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Agila arbetsmetoder</li>
+            </div>
+          </ul>
+          <ul>
+            <li className='underline absolute'>Introducerade tekniker:</li>
+            <div className='flex gap-2 pt-6'>
+              <Play width={6} className='text-secondary-color' />
+              <li>PHP</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>MySQL</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>P5.js</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Svelte</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Wordpress</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>Jest</li>
+            </div>
+            <div className='flex gap-2'>
+              <Play width={6} className='text-secondary-color' />
+              <li>React TL</li>
+            </div>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
