87f51b25f53dec573edfb371ae1ba68e18f7f1f5
Simon <sillen.00@hotmail.com>
2024-05-23 21:30:38 +0200

    Experience done!?!

--- app/containers/home-page/Experience.tsx
+++ app/containers/home-page/Experience.tsx
@@ -218,8 +218,18 @@ const Experience = () => {
               </h4>
               <p className='fira small gray pb-4 pt-0.5'>juni - augusti 2019</p> {/* 29 januari - 17 maj 2024 */}
               <div className='flex gap-4'>
-                <Play width={7} className='text-secondary-color' />
-                <p className='gray'>Här var jag hos kokokaka</p>
+                <Play width={7} className='text-secondary-color flex-none' />
+                <p className='gray'>
+                  Efter min gymnasieutbildning hade jag förmånen att arbeta som butiksäljare på Netto, där jag fick möjlighet att utveckla
+                  färdigheter inom varulagerhantering, kassahantering, städning, underhåll och kundservice.
+                </p>
+              </div>
+              <div className='flex gap-4 pt-4'>
+                <Play width={7} className='text-secondary-color flex-none' />
+                <p className='gray'>
+                  Jag lärde mig vikten av effektiv kommunikation, samarbete och problemlösning, samt hur man hanterar krav och förväntningar
+                  från olika kundgrupper.
+                </p>
               </div>
             </div>
           )}
