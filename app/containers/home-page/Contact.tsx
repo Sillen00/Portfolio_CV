72676105cf8756dcf1863e6341e74b147bdeeff8
Simon <sillen.00@hotmail.com>
2024-05-23 22:16:02 +0200

    start contact page.

--- app/containers/home-page/Contact.tsx
+++ app/containers/home-page/Contact.tsx
@@ -1,11 +1,24 @@
+import OrangeOutlineBtn from "@/app/components/OrangeOutlineBtn";
+import Link from "next/link";
+
 const Contact = () => {
   return (
-    <section className='max-w-screen-lg mx-auto px-6 md:px-28'  id='contact'>
-      <h2>Contact</h2>
-      <h2>Contact</h2>
-      <h2>Contact</h2>
-      <h2>Contact</h2>
-    </section>
+    <>
+      <section className='max-w-screen-lg mx-auto pb-60 px-6 md:px-28' id='contact'>
+        <h4 className={"fira text-center text-secondary-color"}>04. Contact</h4>
+        <h3 className='font-semibold text-center my-4'>Kom I Kontakt</h3>
+        <p className='text-center pb-8 max-w-[600px] mx-auto'>
+          Jag söker för närvarande nya möjligheter. Min inkorg alltid öppen, oavsett om du har en fråga eller bara vill säga hej så ska jag
+          göra mitt bästa för att återkomma till dig!
+        </p>
+        <div className='flex justify-center pt-4'>
+          <Link href='mailto:simonbengtsson00@gmail.com' target='_blank'>
+            <OrangeOutlineBtn className='fira px-6 py-4'>Say Hello</OrangeOutlineBtn>
+          </Link>
+        </div>
+      </section>
+      <p className='fira small text-center text-secondary-color'>© 2024 av Simon Bengtsson</p>
+    </>
   );
 };
 
--- app/containers/home-page/Portfolio.tsx
+++ app/containers/home-page/Portfolio.tsx
@@ -1,10 +1,12 @@
+import { twMerge } from "tailwind-merge";
+import css from "./_Home.module.scss";
+
 const Portfolio = () => {
   return (
-    <section className='max-w-screen-lg mx-auto px-6 md:px-28' id='portfolio'>
-      <h2>Portfolio</h2>
-      <h2>Portfolio</h2>
-      <h2>Portfolio</h2>
-      <h2>Portfolio</h2>
+    <section className='max-w-screen-lg mx-auto pb-60 px-6 md:px-28' id='portfolio'>
+      <h3 className={twMerge(css.line, "flex flex-nowrap items-center relative mb-8")}>
+        <span className='fira text-secondary-color text-2xl mr-4'>03. </span>Portfolio
+      </h3>
     </section>
   );
 };
