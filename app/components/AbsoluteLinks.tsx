9a930f547325a9c886475f7f8049accad7345cc2
Simon <sillen.00@hotmail.com>
2024-05-23 21:41:24 +0200

    fix on absolute links.

--- app/components/AbsoluteLinks.tsx
+++ app/components/AbsoluteLinks.tsx
@@ -6,20 +6,20 @@ import css from "./AbsoluteLinks.module.scss";
 const AbsoluteLinks = () => {
   return (
     <>
-      <div className={twMerge(css.lineSocials, 'hidden fixed bottom-32 left-12 z-10 md:flex flex-col gap-4')}>
-        <Link className='hover:text-secondary-color' href='github'>
+      <div className={twMerge(css.lineSocials, "hidden fixed bottom-32 left-12 z-10 md:flex flex-col gap-4")}>
+        <Link className={twMerge(css.iconHover, "hover:text-secondary-color")} href='github'>
           <Github />
         </Link>
-        <Link className='hover:text-secondary-color' href='linked'>
+        <Link className={twMerge(css.iconHover, "hover:text-secondary-color")} href='linked'>
           <Linkedin />
         </Link>
-        <Link className='hover:text-secondary-color' href='telefon'>
+        <Link className={twMerge(css.iconHover, "hover:text-secondary-color")} href='telefon'>
           <Phone />
         </Link>
       </div>
-      <div className={twMerge(css.lineEmail, 'hidden md:block fixed bottom-60 -right-16 z-10 rotate-90')}>
-        <Link className='hover:text-secondary-color' href='mail'>
-          simonbengtsson00@gmail.com
+      <div className={twMerge(css.lineEmail, "hidden md:block fixed bottom-60 -right-16 z-10 rotate-90")}>
+        <Link className={twMerge("hover:text-secondary-color")} href='mail'>
+          <p className={css.onHover}>simonbengtsson00@gmail.com</p>
         </Link>
       </div>
     </>
