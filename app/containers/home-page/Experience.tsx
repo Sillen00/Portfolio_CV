746d88b92021a8f6ff3f4dd7eea3c24be928d41d
Simon <sillen.00@hotmail.com>
2024-05-23 21:11:21 +0200

    Gym control och medieinstitutet text sectionen.

--- app/containers/home-page/Experience.tsx
+++ app/containers/home-page/Experience.tsx
@@ -118,9 +118,11 @@ const Experience = () => {
               <div className='flex gap-4 pt-4'>
                 <Play width={7} className='text-secondary-color flex-none' />
                 <p className='gray'>
-                  Vid avslutet av min praktik fick jag en mycket positiv utvärdering från Johan: &quot;Simon står på tårna och lär sig fort
-                  och kommer bli en tillgång för den som får chans att arbeta/anställa honom i framtiden. Bästa av alla praktikanter vi haft
-                  i företaget genom åren.&quot;
+                  Vid avslutet av min praktik fick jag en mycket positiv utvärdering från Johan:{" "}
+                  <span className='text-primary'>
+                    &quot;Simon står på tårna och lär sig fort och kommer bli en tillgång för den som får chans att arbeta/anställa honom i
+                    framtiden. Bästa av alla praktikanter vi haft i företaget genom åren.&quot;
+                  </span>
                 </p>
               </div>
             </div>
@@ -141,7 +143,30 @@ const Experience = () => {
               <p className='fira small gray pb-4 pt-0.5'>september 2022 - maj 2024</p> {/* 12 september 2022 - 17 maj 2024 */}
               <div className='flex gap-4'>
                 <Play width={7} className='text-secondary-color' />
-                <p className='gray'></p>
+                <p className='gray'>
+                  Under min utbildning har jag kontinuerligt utvecklat mina tekniska färdigheter och vuxit både personligen och
+                  professionellt. Jag har lärt mig att skapa responsiva och användarvänliga webbplatser samt förmåga att lösa problem,
+                  samarbeta effektivt i team och kommunicera tydligt. Mina kunskaper kommer vara ovärderliga i min framtida karriär
+                </p>
+              </div>
+              <div className='flex gap-4'>
+                <Play width={7} className='text-secondary-color' />
+                <p className='gray'>
+                  När jag blickar framåt ser jag många möjliga yrkesvägar. Att jämföra min nuvarande kunskap med den jag hade när jag
+                  började utbildningen ger mig en känsla av stolthet.Utbildningen har varit en ovärderlig del av min resa mot att bli en
+                  framgångsrik Front End Developer, och jag ser med spänning fram emot vad framtiden har att erbjuda.
+                </p>
+              </div>
+              <div className='flex gap-4 pt-4'>
+                <Play width={7} className='text-secondary-color flex-none' />
+                <p className='gray'>
+                  Vid avslutet av min utbildning fick jag en positiv text av min utbildningsledare Johanna:{" "}
+                  <span className='text-primary'>
+                    &quot;Du är grym och imponerar ständigt! Har varit så kul att följa dig under utbildningens gång. Jag upplever också att
+                    du har utvecklats mycket under din LIA, vilket är så kul att se. Fortsätt att tro på dig själv och gå ut på
+                    arbetsmarknaden med självförtroende.&quot;
+                  </span>
+                </p>
               </div>
             </div>
           )}
