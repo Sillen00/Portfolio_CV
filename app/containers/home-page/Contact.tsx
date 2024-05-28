7c1c808edc4cca802cd75044e4a4a9e8c8924174
Simon <sillen.00@hotmail.com>
2024-05-28 22:06:24 +0200

    styling contact text.,

--- app/containers/home-page/Contact.tsx
+++ app/containers/home-page/Contact.tsx
@@ -6,8 +6,8 @@ const Contact = () => {
     <section className='max-w-screen-lg mx-auto pt-32 pb-60 px-6 md:px-28' id='contact'>
       <h4 className={"fira text-center text-secondary-color"}>04. Contact</h4>
       <h3 className='font-semibold text-center my-4'>Kom I Kontakt</h3>
-      <p className='text-center pb-8 max-w-[600px] mx-auto'>
-        Jag söker för närvarande nya möjligheter. Min inkorg alltid öppen, oavsett om du har en fråga eller bara vill säga hej så ska jag
+      <p className='text-center pb-8 max-w-[580px] mx-auto'>
+        Jag söker nya möjligheter. Min inkorg alltid öppen, oavsett om du har en fråga eller bara vill säga hej så ska jag
         göra mitt bästa för att återkomma till dig!
       </p>
       <div className='flex justify-center pt-4'>
