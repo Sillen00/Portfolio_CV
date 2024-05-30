e2dc3efaafb3b6f734823d066d42090d1122259c
Simon <sillen.00@hotmail.com>
2024-05-30 21:59:45 +0200

    footer hover link..

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <Link href='https://github.com/Sillen00'>
-      <p className='fira small text-center hover:text-secondary-color pb-8'>© 2024 av Simon Bengtsson</p>;
+      <p className='fira w-[280px] small mx-auto text-center hover:text-secondary-color transition-all duration-300 pb-8'>© 2024 av Simon Bengtsson</p>
     </Link>
   );
 };
