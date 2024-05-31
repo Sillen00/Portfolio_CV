a088d0677b269653f172f062701a49255f836ece
Simon <sillen.00@hotmail.com>
2024-05-31 20:46:41 +0200

    fix close button on mobile.

--- app/components/PDFViewer.tsx
+++ app/components/PDFViewer.tsx
@@ -8,8 +8,8 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ src, onClose }) => {
     <div className='fixed z-50 inset-0 bg-black bg-opacity-60 flex justify-center items-center'>
       <div className='relative w-11/12 sm:w-10/12 h-5/6'>
         <iframe src={src} className='w-full h-full' />
-        <button onClick={onClose} className='absolute top-3 -right-14 bg-red-500 text-white p-1 rounded'>
-          Stäng
+        <button onClick={onClose} className='absolute top-3 left-3 sm:top-3 sm:left-auto sm:-right-14 bg-red-500 text-white p-1 rounded'>
+          Close
         </button>
       </div>
     </div>
