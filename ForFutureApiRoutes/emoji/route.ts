855c54f5a5947dfb4afbba5a5a765d4b115fca3a
Simon <sillen.00@hotmail.com>
2024-06-01 19:43:12 +0200

    New try to comment out backend for now.

---
+++ ForFutureApiRoutes/emoji/route.ts
@@ -0,0 +1,28 @@
+// import prisma from "@/prisma/client";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function POST(req: NextRequest): Promise<NextResponse> {
+//   const { emoji } = await req.json();
+//   try {
+//     const newEmoji = await prisma.emoji.create({
+//       data: {
+//         emoji: emoji,
+//       },
+//     });
+
+//     return NextResponse.json(
+//       { emoji: newEmoji },
+//       {
+//         status: 201,
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//   } catch (error) {
+//     return NextResponse.json(
+//       { error: "Failed to submit emoji" },
+//       { status: 500 }
+//     );
+//   }
+// }
