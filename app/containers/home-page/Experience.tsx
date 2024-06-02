348aa9a4bd9fea9ed7e4fd4a008ab40b88d31d0e
Simon <sillen.00@hotmail.com>
2024-06-02 21:47:01 +0200

    made a array out of the experiences also.

--- app/containers/home-page/Experience.tsx
+++ app/containers/home-page/Experience.tsx
@@ -10,6 +10,8 @@ import Netto from "./experience/Netto";
 const Experience = () => {
   const [selectedItem, setSelectedItem] = useState("Kokokaka");
 
+  const experiences = ["Kokokaka", "Gym Control", "MedieInstitutet", "Elpro", "Netto"];
+
   return (
     <section className='max-w-screen-lg mx-auto pt-32 pb-28 px-6 md:px-28' id='experience'>
       <h3 className={twMerge(css.line, "flex flex-nowrap items-center relative mb-8")}>
@@ -18,24 +20,15 @@ const Experience = () => {
 
       <div className='sm:flex'>
         <ul className={twMerge(css.navUl, "fira flex text-nowrap overflow-x-scroll overflow-y-hidden sm:flex-col sm:overflow-x-hidden")}>
-          <li className={twMerge(css.navItem, selectedItem == "Kokokaka" && css.active)} onClick={() => setSelectedItem("Kokokaka")}>
-            Kokokaka
-          </li>
-          <li className={twMerge(css.navItem, selectedItem == "Gym Control" && css.active)} onClick={() => setSelectedItem("Gym Control")}>
-            Gym Control
-          </li>
-          <li
-            className={twMerge(css.navItem, selectedItem == "MedieInstitutet" && css.active)}
-            onClick={() => setSelectedItem("MedieInstitutet")}
-          >
-            Medieinstitutet
-          </li>
-          <li className={twMerge(css.navItem, selectedItem == "Elpro" && css.active)} onClick={() => setSelectedItem("Elpro")}>
-            Elpro
-          </li>
-          <li className={twMerge(css.navItem, selectedItem == "Netto" && css.active)} onClick={() => setSelectedItem("Netto")}>
-            Netto
-          </li>
+          {experiences.map(experience => (
+            <li
+              key={experience}
+              className={twMerge(css.navItem, selectedItem === experience && css.active)}
+              onClick={() => setSelectedItem(experience)}
+            >
+              {experience}
+            </li>
+          ))}
         </ul>
 
         <div className='pt-6 sm:pt-0 sm:ml-4 flex-1'>
