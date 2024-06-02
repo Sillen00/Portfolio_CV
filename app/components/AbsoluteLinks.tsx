a47d1338c2c62578b736735aadcaee3987bf7f52
Simon <sillen.00@hotmail.com>
2024-06-02 21:24:57 +0200

    vissually hidden link texts for screan readers.

--- app/components/AbsoluteLinks.tsx
+++ app/components/AbsoluteLinks.tsx
@@ -7,8 +7,14 @@ const AbsoluteLinks = () => {
   return (
     <>
       <div className={twMerge(css.lineSocials, "hidden fixed bottom-32 left-12 z-10 md:flex flex-col gap-4")}>
-        <Link className={twMerge(css.iconHover, "hover:text-secondary-color")} href='https://github.com/Sillen00' target='_blank'>
+        <Link
+          className={twMerge(css.iconHover, "hover:text-secondary-color")}
+          href='https://github.com/Sillen00'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           <Github />
+          <span className={css.visuallyHidden}>GitHub Profile</span>
         </Link>
         <Link
           className={twMerge(css.iconHover, "hover:text-secondary-color")}
@@ -16,9 +22,11 @@ const AbsoluteLinks = () => {
           target='_blank'
         >
           <Linkedin />
+          <span className={css.visuallyHidden}>LinkedIn Profile</span>
         </Link>
         <Link className={twMerge(css.iconHover, "hover:text-secondary-color")} href='tel:+46706612089'>
           <Phone />
+          <span className={css.visuallyHidden}>Phone Number</span>
         </Link>
       </div>
       <div className={twMerge(css.lineEmail, "hidden md:block fixed bottom-60 -right-16 z-10 rotate-90")}>
