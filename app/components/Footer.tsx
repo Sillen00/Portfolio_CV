2667575e3688b2edd593d7317439a2ba9a568ccd
Simon <sillen.00@hotmail.com>
2024-06-02 21:25:21 +0200

    removed link from footer..

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,8 @@
-import Link from "next/link";
-
 const Footer = () => {
   return (
-    <Link href='https://github.com/Sillen00' target='_blank'>
-      <p className='fira w-[280px] small mx-auto text-center hover:text-secondary-color transition-all duration-300 pb-8'>© 2024 av Simon Bengtsson</p>
-    </Link>
+    <p className='fira w-[280px] small mx-auto text-center hover:text-secondary-color transition-all duration-300 pb-8'>
+      © 2024 av Simon Bengtsson
+    </p>
   );
 };
 
