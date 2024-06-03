483ddc7f837058fe1df0d02deef63ac180401ae2
Simon <sillen.00@hotmail.com>
2024-06-03 21:10:29 +0200

    text gray contact section.

--- app/containers/home-page/Contact.tsx
+++ app/containers/home-page/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <section className='max-w-screen-lg mx-auto pt-52 pb-60 px-6 md:px-28' id='contact'>
       <p className={"fira text-center text-secondary-color"}>04. What&rsquo;s Next?</p>
       <h3 className='font-semibold text-center my-4'>Get In Touch</h3>
-      <p className='text-center pb-8 max-w-[580px] mx-auto'>
+      <p className='text-gray text-center pb-8 max-w-[580px] mx-auto'>
         Jag söker nya möjligheter. Min inkorg alltid öppen, oavsett om du har en fråga eller bara vill säga hej så ska jag göra mitt bästa
         för att återkomma till dig!
       </p>
