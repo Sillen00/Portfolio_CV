b3a97851a91a0dd18904f405a3a44937b7421bb8
Simon <sillen.00@hotmail.com>
2024-06-08 15:41:08 +0200

    experience elpro section translation eng + swe.

--- app/containers/home-page/experiencecomponents/Elpro.tsx
+++ app/containers/home-page/experiencecomponents/Elpro.tsx
@@ -1,37 +1,32 @@
 import { Play } from "lucide-react";
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import css from "../_Home.module.scss";
 
 const Elpro = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <h4>
-        Elmontör{" "}
+        {t("experience.elpro.title")}{" "}
         <Link className='text-secondary-color' href={"https://elpro.se/"} target='_blank'>
           @ Elpro i Alingsås
         </Link>
       </h4>
-      <p className='fira small text-gray pb-4 pt-0.5'>september 2019 - september 2022</p> {/* 29 januari - 17 maj 2024 */}
+      <p className='fira small text-gray pb-4 pt-0.5'>{t("experience.elpro.date")}</p> {/* 29 januari - 17 maj 2024 */}
       <div className={twMerge(css.textRowHover, "flex gap-4")}>
         <Play width={11} className='text-secondary-color flex-none' />
-        <p className='text-gray'>
-          Under min tid på Elpro har jag fått en stor förståelse för industriell automation och utvecklat mina färdigheter inom
-          elkonstruktion. Jag arbetade som elmontör, där jag spelade en viktig roll i produktionen av avancerade elektriska styrsystem och
-          apparatskåp.
-        </p>
+        <p className='text-gray'>{t("experience.elpro.description1")}</p>
       </div>
       <div className={twMerge(css.textRowHover, "flex gap-4 pt-4")}>
         <Play width={11} className='text-secondary-color flex-none' />
-        <p className='text-gray'>
-          Med hjälp av olika ritningar skapade jag skräddarsydda lösningar för att möta kundernas specifika behov. Jag hanterade både
-          enstaka projekt och serietillverkning av apparatskåp. Min roll innefattade att bygga system från grunden och säkerställa att varje
-          projekt uppfyllde höga kvalitetsstandarder.
-        </p>
+        <p className='text-gray'>{t("experience.elpro.description2")}</p>
       </div>
       <div className={twMerge(css.textRowHover, "flex gap-4 pt-4")}>
         <Play width={11} className='text-secondary-color flex-none' />
-        <p className='text-gray'>Jag har lärt mig vikten av noggrannhet, effektivitet och kontinuerlig förbättring.</p>
+        <p className='text-gray'>{t("experience.elpro.description3")}</p>
       </div>
     </div>
   );
