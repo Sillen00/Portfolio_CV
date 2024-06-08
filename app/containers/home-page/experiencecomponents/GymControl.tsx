75c33393e9c1121e2af10969484023227d1f0f75
Simon <sillen.00@hotmail.com>
2024-06-08 15:27:58 +0200

    experience gymcontrol translations eng + swe

--- app/containers/home-page/experiencecomponents/GymControl.tsx
+++ app/containers/home-page/experiencecomponents/GymControl.tsx
@@ -1,33 +1,29 @@
 import { Play } from "lucide-react";
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 import { twMerge } from "tailwind-merge";
 import css from "../_Home.module.scss";
 
 const GymControl = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <h4>
-        Fullstack Utvecklare{" "}
+        {t("experience.gymControl.title")}{" "}
         <Link className='text-secondary-color' href={"https://gymcontrol.se/"} target='_blank'>
           @ Gym Control
         </Link>
       </h4>
-      <p className='fira small text-gray pb-4 pt-0.5'>september - maj 2023</p> {/* 11 september - 17 maj 2023 */}
+      <p className='fira small text-gray pb-4 pt-0.5'>{t("experience.gymControl.date")}</p> {/* 11 september - 17 maj 2023 */}
       <div className={twMerge(css.textRowHover, "flex gap-4")}>
         <Play width={11} className='text-secondary-color flex-none' />
-        <p className='text-gray'>
-          Hos Gym Control fick jag möjlighet att ytterligare utveckla mina kunskaper inom frontend-teknologier såsom HTML, CSS,
-          Javascript/JQuery samt integrationer med olika API:er och PHP med SQL som backend.
-        </p>
+        <p className='text-gray'>{t("experience.gymControl.description1")}</p>
       </div>
       <div className={twMerge(css.textRowHover, "flex gap-4 pt-4")}>
         <Play width={11} className='text-secondary-color flex-none' />
         <p className='text-gray'>
-          Vid avslutet av min praktik fick jag en mycket positiv utvärdering från Johan:{" "}
-          <span className='text-text-color'>
-            &quot;Simon står på tårna och lär sig fort och kommer bli en tillgång för den som får chans att arbeta/anställa honom i
-            framtiden. Bästa av alla praktikanter vi haft i företaget genom åren.&quot;
-          </span>
+          {t("experience.gymControl.description2")} <span className='text-color'>{t("experience.gymControl.quote")}</span>
         </p>
       </div>
     </div>
