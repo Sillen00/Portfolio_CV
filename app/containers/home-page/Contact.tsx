72eb198ae27c7cc725193d7cc27fdfaf8ba201df
Simon <sillen.00@hotmail.com>
2024-06-08 16:00:55 +0200

    contact section translated eng + swe done.

--- app/containers/home-page/Contact.tsx
+++ app/containers/home-page/Contact.tsx
@@ -1,18 +1,19 @@
 import OrangeOutlineBtn from "@/app/components/OrangeOutlineBtn";
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+  const { t } = useTranslation();
+
   return (
     <section className='max-w-screen-lg mx-auto pt-52 pb-60 px-6 md:px-28' id='contact'>
       <p className={"fira text-center text-secondary-color"}>04. What&rsquo;s Next?</p>
-      <h3 className='font-semibold text-center my-4'>Get In Touch</h3>
-      <p className='text-gray text-center pb-8 max-w-[580px] mx-auto'>
-        Jag söker nya möjligheter. Min inkorg alltid öppen, oavsett om du har en fråga eller bara vill säga hej så ska jag göra mitt bästa
-        för att återkomma till dig!
-      </p>
+      <h3 className='font-semibold text-center my-4'>{t("contact.getInTouch")}</h3>
+      <p className='text-gray text-center pb-8 max-w-[580px] mx-auto'>{t("contact.endingText")}</p>
+
       <div className='flex justify-center pt-4'>
         <Link href='mailto:simonbengtsson00@gmail.com' target='_blank'>
-          <OrangeOutlineBtn className='fira px-6 py-4'>Say Hello</OrangeOutlineBtn>
+          <OrangeOutlineBtn className='fira px-6 py-4'>{t("contact.contactMe")}</OrangeOutlineBtn>
         </Link>
       </div>
     </section>
