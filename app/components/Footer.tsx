ce2bce162f5efa4b647e0ee99e15a6064a3acf25
Simon <sillen.00@hotmail.com>
2024-06-10 21:26:08 +0200

    tested to fix the i18next errors.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,12 @@
+// import { useTranslation } from "react-i18next";
+
 const Footer = () => {
+  // const { t } = useTranslation();
+
   return (
     <p className='fira w-[280px] small mx-auto text-center hover:text-secondary-color transition-all duration-300 pb-8'>
-      © 2024 av Simon Bengtsson
+      {/* {t("footer.rightsReserved")} */}
+      hhhhmmm
     </p>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import "./services/i18n";
 import About from "./containers/home-page/About";
 import Contact from "./containers/home-page/Contact";
 import Experience from "./containers/home-page/Experience";
 import Hero from "./containers/home-page/Hero";
 import Portfolio from "./containers/home-page/Portfolio";
+// import "./services/i18n";
 
 export default function Home() {
   return (
